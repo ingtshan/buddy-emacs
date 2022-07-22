ee0f88d90ef9f2f714b44a96d5458503f1146da9
ingtshan <rongcanyo@gmail.com>
2022-07-22 23:44:02 +0800

    Assimilate evil, vertico, which-key

--- init.el
+++ init.el
@@ -19,7 +19,33 @@
 
 ;;; my better default
 ;;
+(require 'better-defaults)
 (require 'mac-command)  ; use some mac shortcut
+
+;;; tips about loading order
+;; file: early-init.el
+;; file: init.el
+;; `after-init-hook'
+;; `emacs-startup-hook'
+
+;;; evil
+;; bring vim to emacs
+(add-hook 'after-init-hook #'(lambda () (require 'evil) (evil-mode 1)))
+
+;;; which-key
+;; make my key-binding clear
+(with-eval-after-load 'evil
+  (diminish 'which-key-mode)
+  (which-key-setup-side-window-bottom))
+
+;;; vertico, orderless
+;; completion UI
+(add-hook 'after-init-hook 'vertico-mode)
+(with-eval-after-load 'vertico
+  (require 'orderless)
+  (setq completion-styles '(orderless basic)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion)))))
 
 ;; Local Variables:
 ;; indent-tabs-mode: nil
