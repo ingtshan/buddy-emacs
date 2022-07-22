e5503df14ea2877615f4854ceb22618c07a70920
ingtshan <rongcanyo@gmail.com>
2022-07-22 22:27:36 +0800

    Assimilate outshine and epkg with his dependence

--- init.el
+++ init.el
@@ -2,12 +2,20 @@
 
 ;;; Commentary:
 
-;;; Code:
+;;; Code
 
-(eval-and-compile ; `borg'
+;;; borg
+;; use git submodule to maintain my package
+(eval-and-compile 
   (add-to-list 'load-path (expand-file-name "lib/borg" user-emacs-directory))
   (require 'borg)
   (borg-initialize))
+
+;;; epkag
+;; describe dependence of a given package
+(require 'epkg)
+(setq epkg-repository (expand-file-name "etc/epkgs" user-emacs-directory))
+(setq epkg-database-connector 'sqlite-builtin)
 
 ;; Local Variables:
 ;; indent-tabs-mode: nil
