777560d4edbb4f8826d2e4034c5020407c2a4128
ingtshan <rongcanyo@gmail.com>
2022-07-23 00:15:13 +0800

    custom my better default

--- init.el
+++ init.el
@@ -19,8 +19,56 @@
 
 ;;; my better default
 ;;
-(require 'better-defaults)
 (require 'mac-command)  ; use some mac shortcut
+(progn
+  (unless (memq window-system '(mac ns))
+    (menu-bar-mode -1))
+  (when (fboundp 'tool-bar-mode)
+    (tool-bar-mode -1))
+  (when (fboundp 'scroll-bar-mode)
+    (scroll-bar-mode -1))
+  (when (fboundp 'horizontal-scroll-bar-mode)
+    (horizontal-scroll-bar-mode -1))
+
+  (autoload 'zap-up-to-char "misc"
+    "Kill up to, but not including ARGth occurrence of CHAR." t)
+
+  (require 'uniquify)
+  (setq uniquify-buffer-name-style 'forward)
+
+  ;; https://www.emacswiki.org/emacs/SavePlace
+  (save-place-mode 1)
+
+  (set-default-coding-systems 'utf-8)
+
+  ;; (global-set-key (kbd "M-/") 'hippie-expand)
+  ;; (global-set-key (kbd "C-x C-b") 'ibuffer)
+  ;; (global-set-key (kbd "M-z") 'zap-up-to-char)
+  ;; (global-set-key (kbd "C-s") 'isearch-forward-regexp)
+  ;; (global-set-key (kbd "C-r") 'isearch-backward-regexp)
+  ;; (global-set-key (kbd "C-M-s") 'isearch-forward)
+  ;; (global-set-key (kbd "C-M-r") 'isearch-backward)
+
+  (show-paren-mode 1)
+  (setq-default indent-tabs-mode nil)
+  (savehist-mode 1)
+  (setq use-short-answers t
+        confirm-kill-emacs 'y-or-n-p
+        save-interprogram-paste-before-kill t
+        apropos-do-all t
+        mouse-yank-at-point t
+        require-final-newline t
+        visible-bell t
+        load-prefer-newer t
+        backup-by-copying t
+        create-lockfiles nil
+        make-backup-files nil
+        ediff-window-setup-function 'ediff-setup-windows-plain
+        custom-file (expand-file-name "custom.el" user-emacs-directory))
+
+  (unless backup-directory-alist
+    (setq backup-directory-alist `(("." . ,(concat user-emacs-directory
+                                                   "backups"))))))
 
 ;;; tips about loading order
 ;; file: early-init.el
